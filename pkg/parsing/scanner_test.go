eaf8b2c0175d56e645497c9b1d0226132cf54f3d
bloshko <mis.bloko@gmail.com>
2025-05-23 17:16:29 +0200

    Test token scanner

--- pkg/parsing/scanner_test.go
+++ pkg/parsing/scanner_test.go
@@ -1,12 +1,58 @@
 package parsing
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestNewScanner(t *testing.T) {
 	source := "int main {}"
 	scanner := *NewScanner(source)
 
-	if !(scanner.Source == source && scanner.Tokens == nil && scanner.start == 0 && scanner.current == 0) {
-		t.Errorf("Scanner created incorrectly")
-	}
+	assert.Equal(t, scanner, Scanner{
+		Source:  source,
+		Tokens:  nil,
+		start:   0,
+		current: 0,
+		line:    1,
+	})
+
+}
+
+func TestScanTokens(t *testing.T) {
+	source := "int main {>=12}"
+	scanner := *NewScanner(source)
+	scanner.ScanTokens()
+
+	assert.Equal(t, scanner.Tokens, []Token{{
+		Type:    LEFT_BRACE,
+		Lexeme:  "{",
+		Literal: nil,
+		Line:    1,
+	},
+		{
+			Type:    GREATER_EQUAL,
+			Lexeme:  ">=",
+			Literal: nil,
+			Line:    1,
+		},
+		{
+			Type:    NUMBER,
+			Lexeme:  "12",
+			Literal: nil,
+			Line:    1,
+		},
+		{
+			Type:    RIGHT_BRACE,
+			Lexeme:  "}",
+			Literal: nil,
+			Line:    1,
+		},
+		{
+			Type:    EOF,
+			Lexeme:  "",
+			Literal: nil,
+			Line:    1,
+		}})
+
 }
